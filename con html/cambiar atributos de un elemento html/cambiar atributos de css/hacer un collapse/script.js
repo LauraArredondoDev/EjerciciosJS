daec5527899bdf1b01e178737086ea7d29ba955d
LauraArredondoDev <lauraarredondodev@gmail.com>
2024-06-09 16:45:00 +0200

    Resuelto el ejercicio de hacer un collapse

---
+++ con html/cambiar atributos de un elemento html/cambiar atributos de css/hacer un collapse/script.js
@@ -0,0 +1,12 @@
+function expandir() {
+    const caja = document.getElementById('contenedor');
+
+    caja.style.height = '200px';
+    
+}
+
+function colapsar() {
+    const caja = document.getElementById('contenedor');
+
+    caja.style.height = '50px';
+}
