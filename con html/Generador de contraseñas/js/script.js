f3925af6c7848ab9683f5f801f85ae420ef5ed5c
LauraArredondoDev <lauraarredondodev@gmail.com>
2024-09-19 21:31:20 +0200

    Verificación de carácteres +  validación de contraseña

--- con html/Generador de contraseñas/js/script.js
+++ con html/Generador de contraseñas/js/script.js
@@ -2,7 +2,7 @@ let cantidad = document.getElementById('cantidad');
 let boton = document.getElementById('generar');
 let contrasena = document.getElementById('contrasena');
 
-const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwyz0123456789';
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwyz0123456789!@#$%^&*()';
 
 function generar() {
     
@@ -10,6 +10,7 @@ function generar() {
 
     if(numeroDigitado < 8){
         alert('La cantidad de caracteres tiene que ser mayor que 8');
+        return;
     }
 
     let password ='';
@@ -17,17 +18,37 @@ function generar() {
     for(let i = 0; i < numeroDigitado; i++ ) {
 
         let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
-
-        console.log(caracterAleatorio);
-
+        
         password += caracterAleatorio;
 
     }
 
+    //Verificar tipos de carácteres
+    let tieneMayusculas = /[A-Z]/.test(password);
+    let tieneMinusculas = /[a-z]/.test(password);
+    let tieneNumeros = /[0-9]/.test(password);
+    let tieneSimbolos = /[\W_]/.test(password);
+
+
     contrasena.value = password;
+
+    //Validar la fortaleza de la contraseña
+    if(tieneMayusculas && tieneMinusculas && tieneNumeros && tieneSimbolos) {
+        alert('La contraseña es fuerte.');
+
+    }else {
+        alert('La contraseña es débil. Asegúrate de incluir al menos una mayúscula, un número y un símbolo.');
+    }
 }  
 
+//1- Guardarme el boton de limpiar en una const o variable
+//2- Hacer una función que diga que cuando pulse el botón lo que hay dentro del input de la contraseña se borre
 
+const botonLimpiar = document.getElementById('limpiar');
+
+function limpiar() {
+    contrasena.value = '';
+}
 
 
 
